9465f627b2cf1267df4453087ea5962b1be70370
Jodaille <jody.noury@gmail.com>
2021-05-04 11:16:57 +0200

    add first parameter for commands id

--- sketchbook/CoordonneesSerie/CoordonneesSerie.ino
+++ sketchbook/CoordonneesSerie/CoordonneesSerie.ino
@@ -1,15 +1,26 @@
 /**
-* Envoi de deux coordonnées sous la forme: 100,50
+* Envoi de deux coordonnées sous la forme: 1,100,50
 * suivi d'une validation par la touche Entrée
+* 
+* la première valeur 1 sera utilisé pour indiquer la 
+* commande à effectuer, si == moveServo alors ce sera
+* un déplacement des servos
+* 
 * les valeurs de 100 et de 50 seront converties en int
 * pour pouvoir être utilisées pour orienter les servos
+* 
 */
 
 String readString; // récupération de la chaîne: String
 
+String command; // données String
 String angleX; // données String
 String angleY; // données String
 
+const int moveServo = 1; // id commande de servo
+
+// Utilisé pour indiquer la commande à effectuer
+int integerCommand = 0;
 // Utilisées pour la conversion
 int integerAngleX = 0;
 int integerAngleY = 0;
@@ -39,27 +50,39 @@ void loop() {
       Serial.print("captured String is : ");
       Serial.println(readString); //prints string to serial port out
 
+    
       ind1 = readString.indexOf(',');  //finds location of first ,
-      angleX = readString.substring(0, ind1);   //captures first data String
       ind2 = readString.indexOf(',', ind1+1 );   //finds location of second ,
-      angleY = readString.substring(ind1+1);
+      
+      command = readString.substring(0, ind1);//captures first data String
+      angleX = readString.substring(ind1+1,ind2);
+      angleY = readString.substring(ind2+1);
 
+      integerCommand = command.toInt();
       integerAngleX = angleX.toInt();
       integerAngleY = angleY.toInt();
 
-      Serial.print("angleX = ");
+      Serial.print("command = ");
+      Serial.print(command);
+      Serial.print(" / angleX = ");
       Serial.print(angleX);
       Serial.print(" / integerAngleX = ");
       Serial.println(integerAngleX);
-      Serial.print("angleY = ");
+      Serial.print("integerCommand = ");
+      Serial.print(integerCommand);
+      Serial.print(" / angleY = ");
       Serial.print(angleY);
       Serial.print(" / integerAngleY = ");
-      Serial.print(integerAngleY);
+      Serial.println(integerAngleY);
 
-      Serial.println();
+      if (integerCommand == moveServo) {
+        Serial.println("Deplacement du servo !");
+      }
+
       Serial.println();
 
       readString=""; //clears variable for new input
+      command="";
       angleX="";
       angleY="";
 
